8708be9416b98b2acc5b5c44d648549fbc8745d2
jokernexus120 <jokernexus120@outlook.com>
2019-10-13 03:51:39 +0800

    20191013-02:33提交 20191013-03:51提交

--- c-haoke-manage-api-server/src/main/java/com/c/haoke/dubbo/server/api/DubboApiApplication.java
+++ c-haoke-manage-api-server/src/main/java/com/c/haoke/dubbo/server/api/DubboApiApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DubboApiApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(DubboApiApplication.class, args);
     }
 }
